dda6f561c444045e7061fe017151b6096be0d165
tarequr <tarequrr8@gmail.com>
2025-08-09 00:04:15 +0600

    chore: create a home, create and detail page

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreatePage() {
+    return (
+        <div>
+            <h1>Create a New Page</h1>
+        </div>
+    )
+}
+
+export default CreatePage
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoteDetailPage() {
+    return (
+        <div>
+            <h1>Note Detail Page</h1>
+        </div>
+    )
+}
+
+export default NoteDetailPage
