583b167792cbc6f766deb1e409e4f5199e69c4de
tarequr <tarequrr8@gmail.com>
2025-08-09 01:10:23 +0600

    chore: install daisyui and configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,14 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 
 function App() {
   return (
-    <div>
+    <div data-theme="forest">
+      <button class="btn">Button</button>
+      <button class="btn btn-neutral">Neutral</button>
+      <button class="btn btn-primary">Primary</button>
+      <button class="btn btn-secondary">Secondary</button>
+      <button class="btn btn-accent">Accent</button>
+      <button class="btn btn-ghost">Ghost</button>
+      <button class="btn btn-link">Link</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
