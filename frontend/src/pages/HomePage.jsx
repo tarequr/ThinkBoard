1919e284ef57d64fe889f0c11f32d7c9deeaa0da
tarequr <tarequrr8@gmail.com>
2025-08-10 22:29:20 +0600

    chore: install npm i cors and try to fetch notes data

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimited from '../components/RateLimited';
+import axios from 'axios';
 
 function HomePage() {
+    const [isRateLimited, setIsRateLimited] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [notes, setNotes] = useState([]);
+
+    // Simulate fetching notes
+    useEffect(() => {
+        const fetchNotes = async () => {
+            console.log('Fetching notes...');
+            try {
+                const response = await axios.get("http://localhost:3000/api/notes");
+                // const data = await response.json();
+                const data = response.data;
+                console.log('Notes fetched successfully:', data);
+                setNotes(data);
+                setIsLoading(false);
+            } catch (error) {
+                setError(error);
+                setIsLoading(false);
+            }
+        };
+
+        fetchNotes();
+    }, []);
+
+    // useEffect(() => {
+    //     if (notes.length > 10) {
+    //         setIsRateLimited(true);
+    //     }
+    // }, [notes]);
+
     return (
         <div className='min-h-screen'>
             <Navbar />
+
+            {isRateLimited && <RateLimited />}
         </div>
     )
 }
