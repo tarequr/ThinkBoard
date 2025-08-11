20604dd5f95d9c1a131a132a680caf0bdc8b0604
tarequr <tarequrr8@gmail.com>
2025-08-11 22:55:19 +0600

    chore: note deleted done

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,9 +2,29 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils'
+import toast from 'react-hot-toast';
+import api from '../lib/axios';
 
 
-function NoteCard({ note }) {
+function NoteCard({ note, setNotes }) {
+    const handleDelete = async (event, noteId) => {
+        event.preventDefault();
+        console.log("Deleting note with ID:", noteId);
+
+        if (!window.confirm("Are you sure you want to delete this note?")) {
+            return;
+        }
+
+        try {
+            await api.delete(`/notes/${noteId}`);
+            setNotes(prevNotes => prevNotes.filter(note => note._id !== noteId));
+            toast.success('Note deleted successfully');
+        } catch (error) {
+            console.error('Error deleting note:', error);
+            toast.error('Failed to delete note');
+        }
+    };
+
     return (
         <Link to={`/note/${note._id}`} className='card bg-base-100 hover:shadow-lg transition-all duration-200 
       border-t-4 border-solid border-[#00FF9D]'>
@@ -16,7 +36,7 @@ function NoteCard({ note }) {
 
                     <div className='flex items-center gap-1'>
                         <PenSquareIcon className='size-4' />
-                        <button className='btn btn-ghost btn-xs text-error'>
+                        <button className='btn btn-ghost btn-xs text-error' onClick={(e) => handleDelete(e, note._id)}>
                             <Trash2Icon className='size-4' />
                         </button>
                     </div>
