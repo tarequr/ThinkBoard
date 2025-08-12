e0f2c51128d1358bba2f6d1c0a7ee1c91e7050e1
tarequr <tarequrr8@gmail.com>
2025-08-12 22:06:37 +0600

    chore: create file for if note not found

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 import api from '../lib/axios';
+import NotesNotFound from '../components/NotesNotFound';
 
 function HomePage() {
     const [isRateLimited, setIsRateLimited] = useState(false);
@@ -55,6 +56,8 @@ function HomePage() {
 
             <div className='max-w-7xl mx-auto p-4 mt-6'>
                 {isLoading && <div className='text-center text-primary py-10'>Loading notes... ⌛</div>}
+
+                {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
                 {notes.length > 0 && !isRateLimited && (
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
